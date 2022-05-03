bbbfc102b9a9fd3e51ac49e144d6ee587d99306a
rollbear <rollbearwu@qq.com>
2022-05-03 11:06:38 +0800

    删除了一些不必要的控制台输出.

--- SNA/utils.py
+++ SNA/utils.py
@@ -4,7 +4,7 @@ import os
 
 def get_file_size(path):
     file_stats = os.stat(path)
-    print(file_stats)
-    print(f'File Size in Bytes is {file_stats.st_size}')
-    print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+    # print(file_stats)
+    # print(f'File Size in Bytes is {file_stats.st_size}')
+    # print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
     return f"{file_stats.st_size / (1024 * 1024):.4f} MB"
